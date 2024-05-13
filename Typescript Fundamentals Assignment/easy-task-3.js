fb193c2db3fae3e0ee0c29be04acfa9e6d1e1457
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-05-13 23:25:24 +0500

    easy-task-3

---
+++ Typescript Fundamentals Assignment/easy-task-3.js
@@ -0,0 +1,6 @@
+"use strict";
+var totalApples = 10;
+var giveAway = 3;
+console.log(`Initially i have ${totalApples} apples`);
+totalApples -= giveAway;
+console.log(`After give away ${giveAway} apples, ${totalApples} apples are left`);
---
+++ Typescript Fundamentals Assignment/easy-task-3.ts
@@ -0,0 +1,5 @@
+var totalApples: number = 10
+var giveAway: number = 3
+console.log(`Initially i have ${totalApples} apples`) 
+totalApples -= giveAway
+console.log(`After give away ${giveAway} apples, ${totalApples} apples are left`)
