cf65965e9988c0e058aab3cd7b66b2e472565855
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-05-13 23:35:22 +0500

    easy-task-4 deployee

---
+++ Typescript Fundamentals Assignment/easy-task-4.js
@@ -0,0 +1,5 @@
+"use strict";
+var firstName = "Shahmeer";
+var lastName = "Bukhari";
+var fullName = firstName + " " + lastName;
+console.log(`Full Name is: ${fullName}`);
---
+++ Typescript Fundamentals Assignment/easy-task-4.ts
@@ -0,0 +1,4 @@
+var firstName: string = "Shahmeer"
+var lastName: string = "Bukhari"
+var fullName: string = firstName +" "+ lastName
+console.log(`Full Name is: ${fullName}`)
