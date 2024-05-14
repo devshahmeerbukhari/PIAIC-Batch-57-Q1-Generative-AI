ba7af34cbf6c4c51777f37c9514b1c377f85bb3e
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-05-14 09:35:51 +0500

    medium-task-6 deployee

---
+++ Typescript Fundamentals Assignment/medium/medium-task-6.js
@@ -0,0 +1,7 @@
+"use strict";
+function calculateArea(radius) {
+    const pi = 3.144;
+    return (pi * (radius * radius));
+}
+const radius = 12;
+console.log(calculateArea(radius));
---
+++ Typescript Fundamentals Assignment/medium/medium-task-6.ts
@@ -0,0 +1,6 @@
+function calculateArea(radius: number){
+    const pi: any = 3.144
+    return (pi * (radius * radius))
+}
+const radius = 12
+console.log(calculateArea(radius))
