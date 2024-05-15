0ae303a28c88aba47a7c224f7ad3274777fc47ab
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-05-15 10:58:39 +0500

    arrays-and-array-methods-13 deployee

---
+++ Typescript Fundamentals Assignment/Arrays and Array Methods/arrays-and-array-methods-13.ts
@@ -0,0 +1,10 @@
+//arrays-and-array-methods-13.js
+//Develop a function that takes an array of numbers and returns a new array with each number squared.
+function makeSqrOfArraysElement(array: number[]){
+    return array.map((element) => {
+        return (element * element);
+    })
+}let array: number[] = [1, 2, 3, 4, 5, 6]
+console.log(`Original array: ${array}`)
+array = makeSqrOfArraysElement(array)
+console.log(`Updated Array: ${array}`)
