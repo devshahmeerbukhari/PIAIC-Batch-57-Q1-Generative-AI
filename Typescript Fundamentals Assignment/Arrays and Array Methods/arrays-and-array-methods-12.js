1e76ffbf1b0fd0b00e2414f118b735f659dfcce0
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-05-15 10:46:42 +0500

    arrays-and-array-methods-12 deployee

---
+++ Typescript Fundamentals Assignment/Arrays and Array Methods/arrays-and-array-methods-12.js
@@ -0,0 +1,4 @@
+"use strict";
+//Create a script that logs the second to last element of an array named
+let arrayName = ["Syed", "Muhammad", "Shahmeer", "Bukhari"];
+console.log(arrayName[arrayName.length - 2]);
---
+++ Typescript Fundamentals Assignment/Arrays and Array Methods/arrays-and-array-methods-12.ts
@@ -0,0 +1,3 @@
+//Create a script that logs the second to last element of an array named
+let arrayName: string[] = ["Syed", "Muhammad", "Shahmeer", "Bukhari"]
+console.log(arrayName[arrayName.length - 2])
