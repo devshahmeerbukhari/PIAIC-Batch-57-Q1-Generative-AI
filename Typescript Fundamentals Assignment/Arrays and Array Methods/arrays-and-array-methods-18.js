5b72072e01dbcdcd35b497aa1780be074be9f566
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-05-16 08:50:29 +0500

    arrays-and-array-methods-18 deployee

---
+++ Typescript Fundamentals Assignment/Arrays and Array Methods/arrays-and-array-methods-18.js
@@ -0,0 +1,22 @@
+"use strict";
+//Develop a function called sumOfElements that calculates the sum of all elements in an array that are either even or odd, based on a parameter.
+function sumOfElements(array, sumEven) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (sumEven) {
+            if (array[i] % 2 === 0) {
+                sum += array[i];
+            }
+        }
+        else {
+            if (array[i] % 2 !== 0) {
+                sum += array[i];
+            }
+        }
+    }
+    return sum;
+}
+let numberArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(`Sum of even number is: ${sumOfElements(numberArr, true)}`);
+console.log(`Sum of odd number is: ${sumOfElements(numberArr, false)}`);
+console.log(`Total Sum is: ${sumOfElements(numberArr, true) + sumOfElements(numberArr, false)}`);
