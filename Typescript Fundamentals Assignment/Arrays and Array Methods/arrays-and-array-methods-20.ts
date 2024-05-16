ed5a1c055202ed2c7c067522f209e6d3212afbf9
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-05-16 09:06:43 +0500

    arrays-and-array-methods-20 deployee

---
+++ Typescript Fundamentals Assignment/Arrays and Array Methods/arrays-and-array-methods-20.ts
@@ -0,0 +1,13 @@
+//Write a function to find and return the smallest number in an array of integers.
+function findSmallestElement(array: number[]){
+    let smallest = array[0]
+    for(let i = 1; i < array.length; i++){
+        if(smallest > array[i]){
+            smallest = array[i]
+        }
+    }
+    return smallest
+}
+let arrayElements = [1, 2, 6, 4, 3, -1, 5, 6, -138, 34, 543, 138]
+console.log(`Array is: ${arrayElements}`)
+console.log(`Smallest Element in the array is: ${findSmallestElement(arrayElements)}`)
