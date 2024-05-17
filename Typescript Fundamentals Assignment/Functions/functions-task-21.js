c36c6fade7f3ec0c5d908106a9e1076500d54c5d
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-05-17 08:50:16 +0500

    functions-task-21 deployee

---
+++ Typescript Fundamentals Assignment/Functions/functions-task-21.js
@@ -0,0 +1,14 @@
+"use strict";
+//Write a function calculateProduct that takes an array of numbers and returns the product of all elements.
+function calculateProduct(array) {
+    /*var result = array[0]
+    for(let i = 1; i < array.length; i++){
+        result *= array[i]
+    }
+    return result*/
+    /*Here using reduce function, reduce funtion is used when you want single or reduced output
+    For Example, sum of the array, product of the array or occurence of any word in an array*/
+    return array.reduce((accumulator, currentValue) => accumulator *= currentValue, 1);
+}
+let array1 = [1, 2, 3, 4];
+console.log(`Product of array [${array1}] is: ${calculateProduct(array1)}`);
