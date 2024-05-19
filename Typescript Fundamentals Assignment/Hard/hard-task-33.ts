8242b7fd43cadeb38df91f0fb58d93b9a8ca1a3a
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-05-19 10:25:23 +0500

    hard-task-33 deployee

---
+++ Typescript Fundamentals Assignment/Hard/hard-task-33.ts
@@ -0,0 +1,17 @@
+/*Write a function that takes an array of integers and sorts them from smallest to largest.*/
+function sortArr(array: number[]){
+    for(let i = 0; i < array.length; i++){
+        for(let j = i + 1; j < array.length; j++){
+            if(array[i] > array[j]){
+                let temp = array[i]
+                array[i] = array[j]
+                array[j] = temp
+            }
+        }
+    }
+    return array
+}
+let array3: number[] = [5, 4, 3, 6, 8, 7, 9, 10, 22, 11, 3, 2, 1, -138]
+console.log(`Original Array: ${array3}`)
+array3 = sortArr(array3)
+console.log(`Sorted Array: ${array3}`)
