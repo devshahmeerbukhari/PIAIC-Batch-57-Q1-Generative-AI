7d609910d47d61eb342fc753db99e12545062625
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-05-19 10:35:50 +0500

    hard-task-34 deployee

---
+++ Typescript Fundamentals Assignment/Hard/hard-task-34.ts
@@ -0,0 +1,10 @@
+/*Develop a TypeScript program that logs every element of an array in reverse order without using the .reverse() method*/
+function reverseOrder(array: number[]){
+    for(let i = array.length - 1; i >= 0; i--){
+        console.log(array[i] + " ")
+    }
+}
+let array4: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(`Original Order of array: ${array4}`)
+console.log(`Reverse Order of array: `)
+reverseOrder(array4)
