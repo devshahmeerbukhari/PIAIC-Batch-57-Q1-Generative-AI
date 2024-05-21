a1f4962c8ff17a5198d6594a4a4862ea57fea9d1
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-05-21 15:32:59 +0500

    Last Task Yet to DO

--- Typescript Fundamentals Assignment/Hard/hard-task-35.ts
+++ Typescript Fundamentals Assignment/Hard/hard-task-35.ts
@@ -1,2 +1,2 @@
 /*Write a script that simulates a basic calculator. It should take two operands and an operator ('+', '-', '*', '/') from the user, perform the operation, and log the result.*/
-//Last Task
+//Yet to do, Last Task
