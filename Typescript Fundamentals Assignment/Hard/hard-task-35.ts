e0f3a86f0af0d53ef162ef62339f613a749506d8
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-05-22 13:12:21 +0500

    Installed inquirer

--- Typescript Fundamentals Assignment/Hard/hard-task-35.ts
+++ Typescript Fundamentals Assignment/Hard/hard-task-35.ts
@@ -1,2 +1,3 @@
 /*Write a script that simulates a basic calculator. It should take two operands and an operator ('+', '-', '*', '/') from the user, perform the operation, and log the result.*/
-//Yet to do, Last Task.
+
+//Basic Calculator
