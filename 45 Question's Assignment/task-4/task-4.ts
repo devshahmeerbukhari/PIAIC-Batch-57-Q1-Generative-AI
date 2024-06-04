87eb7091f6d72e062282adcb00a6c2878e365eab
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-06-04 11:20:30 +0500

    45 Question's task-4 deployee

---
+++ 45 Question's Assignment/task-4/task-4.ts
@@ -0,0 +1,7 @@
+/*Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. 
+Your output should look something like the following, including the quotation marks:
+Albert Einstein once said, “A person who never made a mistake never tried anything new.”*/
+
+let authorName: string = "ALbert Einstein"
+let quote: string = "A person who never made a mistake never tried anything new."
+console.log(`${authorName} once said, "${quote}"`)
