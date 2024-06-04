6f5cedd53e1368d57447acddd103f1874a32cddb
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-06-04 11:51:39 +0500

    45 Question's task-6 deployee

---
+++ 45 Question's Assignment/task-6/task-6.ts
@@ -0,0 +1,12 @@
+/*Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. 
+Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around 
+the name is displayed. Then print the name after striping the white spaces.*/
+
+let personName: string = "\t\n    Shahmeer       Bukhari             \t\t."
+console.log(`Before stripping whitespaces: ${personName}`)
+let afterStipping: string = personName.replace(/\s+/g, ' ')
+/*This code uses the regular expression \s+ to match one or more whitespace characters, 
+and the g flag to replace all occurrences in the string, not just the first one. 
+The replacement string is a single space, so all whitespace characters are 
+replaced with a single space.*/
+console.log(`After stripping whitespaces: ${afterStipping}`)
