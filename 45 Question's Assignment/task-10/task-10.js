a35a81d12de9f4a28a5e814d8d872384c76e8203
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-06-05 00:54:39 +0500

    45 Question's task-10 deployee

---
+++ 45 Question's Assignment/task-10/task-10.js
@@ -0,0 +1,8 @@
+"use strict";
+/*Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
+If you don’t have anything specific to write because your programs are too simple at this point,
+just add your name and the current date at the top of each program file. Then write one sentence
+describing what the program does.*/
+//Below is the way to print on concole.
+//This is just a random comment
+console.log("Comment");
