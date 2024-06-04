05eefa4eb7672e6c31d11344f746a78f549dc380
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-06-05 00:51:51 +0500

    45 Question's task-9 deployee

---
+++ 45 Question's Assignment/task-9/task-9.js
@@ -0,0 +1,6 @@
+"use strict";
+/*Favorite Number: Store your favorite number in a variable. Then, using that variable,
+create a message that reveals your favorite number. Print that message.*/
+let favNumber = 138;
+let message9 = "Shahmeer's favorite number is: " + favNumber;
+console.log(message9);
---
+++ 45 Question's Assignment/task-9/task-9.ts
@@ -0,0 +1,5 @@
+/*Favorite Number: Store your favorite number in a variable. Then, using that variable, 
+create a message that reveals your favorite number. Print that message.*/
+let favNumber: number = 138
+let message9: string = "Shahmeer's favorite number is: " + favNumber
+console.log(message9)
