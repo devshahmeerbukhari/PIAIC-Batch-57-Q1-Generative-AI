39f42240e91fbb5b23fc632a59d1603fe11f60c2
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-06-06 22:03:09 +0500

    45 question's task-21 deployee

---
+++ 45 Question's Assignment/task-21/task-21.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/*They think of something you could store in a TypeScript Object.
+Write a program that creates Objects containing these items.*/
+let PersonObj = {
+    name: "Shahmeer Bukahri",
+    age: 22,
+    gender: "male"
+};
+console.log(PersonObj);
---
+++ 45 Question's Assignment/task-21/task-21.ts
@@ -0,0 +1,12 @@
+/*They think of something you could store in a TypeScript Object. 
+Write a program that creates Objects containing these items.*/
+let PersonObj:{
+    name: string,
+    age: number, 
+    gender: string
+} = {
+    name: "Shahmeer Bukahri",
+    age: 22,
+    gender: "male"
+}
+console.log(PersonObj)
