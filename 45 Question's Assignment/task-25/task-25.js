d5b7376e25367783a57e57d9fdf6a84181844449
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-06-07 10:39:22 +0500

    45 question's task-25 deployee

---
+++ 45 Question's Assignment/task-25/task-25.js
@@ -0,0 +1,16 @@
+"use strict";
+/*Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called
+alien_color and assign it a value of 'green', 'yellow', or 'red'.
+• Write an if statement to test whether the alien’s color is green. If it is,
+print a message that the player just earned 5 points.
+• Write one version of this program that passes the if test and another that fails.
+(The version that fails will have no output.)*/
+Object.defineProperty(exports, "__esModule", { value: true });
+let alien_color = "Green";
+if (alien_color.toLowerCase() === "green") {
+    console.log("Player just earned 5 points");
+}
+else if (alien_color.toLowerCase() === "red") {
+    //no output
+}
+console.log(`${alien_color.toLowerCase() === "green" ? 'PLayer Just earned 5 point' : 'earned nothing'}`);
