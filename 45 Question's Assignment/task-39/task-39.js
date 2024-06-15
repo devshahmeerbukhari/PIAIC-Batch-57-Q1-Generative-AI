650f295852e50c143861d7f53ff875e7539cc96b
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-06-15 17:10:07 +0500

    task-39 deployee

---
+++ 45 Question's Assignment/task-39/task-39.js
@@ -0,0 +1,14 @@
+"use strict";
+/*City Names: Write a function called city_country() that takes in the name of a
+city and its country. The function should return a string formatted like this:
+"Lahore, Pakistan"
+Call your function with at least three city-country pairs, and print the value
+that’s returned.*/
+Object.defineProperty(exports, "__esModule", { value: true });
+function city_country(name, country) {
+    return `${name}, ${country}`;
+}
+let ret_Value = city_country("Lahore", "Pakistan");
+console.log(ret_Value);
+console.log(city_country("Delhi", "India"));
+console.log(city_country("Islamabad", "Pakistan"));
