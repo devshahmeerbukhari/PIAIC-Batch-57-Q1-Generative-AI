046e76e9837ee7206536307dbaeddd7299b351b5
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-06-29 16:29:49 +0500

    45 Question's Assignment task-45 deployee

--- 45 Question's Assignment/task-45/task-45.ts
+++ 45 Question's Assignment/task-45/task-45.ts
@@ -2,3 +2,32 @@
 a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the 
 function with the required information and two other name-value pairs, such as a color or an optional 
 feature. Print the Object that’s returned to make sure all the information was stored correctly.*/
+
+interface Car {
+    manufacturer: string;
+    model: string;
+    [key: string]: any;  // Allows for any number of additional properties of any type
+}
+
+function storeCarInfo(manufacturer: string, model: string, options: {[key: string]: any}): Car {
+    // Initialize the car object with required properties
+    let car: Car = {
+        manufacturer,
+        model
+    };
+
+    // Add all additional options to the car object
+    for (let key in options) {
+        car[key] = options[key];
+    }
+
+    return car;
+}
+
+// Example usage of the function
+let car = storeCarInfo("Tesla", "Model 3", {
+    color: "red",
+    hasSunroof: true
+});
+
+console.log(car);
